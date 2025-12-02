5657ee2f8b7f099534a197da667d7bafbf4b6cbb
Tewl-hs <loki_420@msn.com>
2025-12-02 04:55:46 -0600

    Day 2

---
+++ Day 2/Program.cs
@@ -0,0 +1,19 @@
+﻿var nums = File.ReadAllText("input.txt").Trim().Split(',')
+    .SelectMany(r => { 
+        var p = r.Split('-'); 
+        long start = long.Parse(p[0]), end = long.Parse(p[1]); 
+        return Enumerable.Range(0, (int)(end - start + 1)).Select(i => start + i); 
+    });
+
+// Mirror numbers
+Console.WriteLine(nums.Where(n => {
+    var s = n.ToString();
+    return s.Length % 2 == 0 && s[..(s.Length / 2)] == s[(s.Length / 2)..];
+}).Sum());
+
+// Repeating pattern numbers
+Console.WriteLine(nums.Where(n => {
+    var s = n.ToString();
+    int L = s.Length;
+    return Enumerable.Range(1, L / 2).Any(k => L % k == 0 && string.Concat(Enumerable.Repeat(s[..k], L / k)) == s);
+}).Sum());
---
+++ README.md
@@ -0,0 +1,12 @@
+# Advent of Code 2025 - C#
+
+### Detailed information can be found at [https://adventofcode.com](https://adventofcode.com) 
+
+## How to run
+
+1. Make sure you have [.NET SDK](https://dotnet.microsoft.com/en-us/download) installed.
+2. Place `input.txt` in the project root.
+3. In the terminal, run:
+
+```bash
+dotnet run
